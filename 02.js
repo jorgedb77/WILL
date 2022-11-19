0d85af99978957da6da16276cf411251b2b36804
jorgedb77 <jorgeluiscarp@gmail.com>
2022-11-19 01:46:37 -0300

    segundo commit

--- 02.js
+++ 02.js
@@ -12,7 +12,15 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var masl = 0;
+  for(var i=0; i<strings.length;i++){
+    if(strings[i].length > strings[masl].length){
+      masl = [i];
+    }
+  }
+  return strings[masl];
 }
+console.log(stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag'])) 
 
 // No modifiques nada debajo de esta linea //
 
